609ac3434a09f0d5d3fe536badaa92a5c5328a16
arnolduis <arnolduis@gmail.com>
2016-02-17 15:10:23 +0100

    RED-4405

--- src/superData.js
+++ src/superData.js
@@ -2,14 +2,18 @@
 "use strict";
 
 var memoryProxy = require("./proxy/memory");
+var restProxy = require("./proxy/rest");
 var store = require("./store/store");
 var model = require("./model/model");
 
 module.exports = {
 	proxy: {
-		memory: memoryProxy
+		memory: memoryProxy,
+		rest: restProxy
 	},
-	model: model,
+	model: {
+		model: model
+	},
 	store: {
 		store: store
 	}
