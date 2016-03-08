80351e53b9f6ff695069b90b1cf3a643365de346
Gyula Nemeth <gyula.nemeth85@gmail.com>
2016-03-08 15:56:55 +0100

    RED-4441

--- spec/proxy/ajaxSpec.js
+++ spec/proxy/ajaxSpec.js
@@ -4,6 +4,35 @@ var superData = require("../../src/superData");
 var ajaxProxy = require("../../src/proxy/ajax");
 
 
+var express = require("express");
+var bodyParser = require("body-parser");
+
+
+function createMockServer(config) {
+	var app = express();
+	app.use(bodyParser.urlencoded({limit: "2mb", extended: true}));
+	app.use(bodyParser.json({limit: "2mb"}));
+
+	for (var prop in config.operations) {
+		var act = config.operations[prop];
+
+		var method = act.method.toLowerCase();
+
+		app[method](act.route, act.callback);
+	}
+
+	app.listen(config.port, function() {
+		config.serverStarted();
+	});
+
+	return {
+		stop: function() {
+			//app.close();
+		}
+	};
+}
+
+
 describe("ajax proxy", function() {
 	it("creator function should be defined", function() {
 		expect(typeof superData.proxy.ajax).toBe("function");
@@ -31,7 +60,7 @@ describe("ajax proxy", function() {
 		}).toThrowError("config.idProperty is mandatory!");
 	});
 
-	/*
+	
 	var proxy = ajaxProxy({
 		idProperty: "id",
 		operations: {
@@ -58,6 +87,36 @@ describe("ajax proxy", function() {
 		}
 	});
 
-	proxyBehaviour("ajax", proxy);
-	*/
+	it("read", function(done) {
+		var callbacks = {
+			read: function(req, res) {
+				res.send({
+					items: [],
+					count: 0
+				});
+			}
+		};
+
+		spyOn(callbacks, "read").and.callThrough();
+
+		var mockServer = createMockServer({
+			operations: {
+				read: {
+					route: "/user",
+					method: "GET",
+					callback: callbacks.read
+				}
+			},
+			serverStarted: function() {
+				proxy.read({}, function() {
+					expect(callbacks.read).toHaveBeenCalled();
+					mockServer.stop();
+					done();
+				});
+			},
+			port: 7357
+		});
+	});
+
+	//proxyBehaviour("ajax", proxy);
 });
