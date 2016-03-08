54e545fd3247f709422cbfccac388f5a3428a079
iTyu <nagyityuistvan@gmail.com>
2016-03-08 11:04:38 +0100

    RED-4499 RED-4516

--- spec/model/propSpec.js
+++ spec/model/propSpec.js
@@ -7,13 +7,50 @@ describe("Test prop should be function....", function() {
 });
 
 describe("Test prop value....", function() {
-	var kalacs = {};
 
-	prop(kalacs, "testProp", {
-		value: 10
+	var propCheck;
+	var change;
+
+	beforeEach(function () {
+		change = {
+			before: function() {
+
+			},
+
+			after: function() {
+
+			}
+		};
+
+		spyOn(change, "before");
+		spyOn(change, "after");
+
+		propCheck = {};
+
+		prop(propCheck, "testProp", {
+			value: "test1",
+			beforeChange: change.before,
+			afterChange: change.after
+		});
 	});
 
-	it("function shuojasmineld be defined", function() {
-		expect(kalacs.testProp).toBe(10);
+	it("prop set get", function() {
+		expect(propCheck.testProp).toBe("test1");
+
+		propCheck.testProp = "test2";
+		expect(propCheck.testProp).toBe("test2");
+	});
+
+	it("before after callback", function () {
+		propCheck.testProp = "test3";
+		expect(change.before).toHaveBeenCalled();
+		expect(change.after).toHaveBeenCalled();
+	});
+
+	it("before after sholud not be called", function () {
+		propCheck.testProp = "test1";
+
+		expect(change.before.calls.count()).toBe(0);
+		expect(change.after.calls.count()).toBe(0);
 	});
 });
